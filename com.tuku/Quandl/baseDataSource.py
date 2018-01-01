99911b91cbb304da84272ff37c0973724bccafa5
Tuku <agoenka.g@gmail.com>
2018-01-01 15:02:51 -0500

    move API key to local cfg

--- com.tuku/Quandl/baseDataSource.py
+++ com.tuku/Quandl/baseDataSource.py
@@ -1,10 +1,10 @@
 import quandl
+import quandlconfig
 
-API_KEY = "FP3E3hX1Mtf3tv6XCj-z"
 OHLC_TABLE = "WIKI/PRICES"
 
 # authenticate with quandl
-quandl.ApiConfig.api_key = API_KEY
+quandl.ApiConfig.api_key = quandlconfig.API_KEY
 
 
 data = quandl.get_table(OHLC_TABLE, date='2017-12-27', qopts = { "columns" : [ "ticker","date","open","high","low","close","volume"]}, paginate = True )
